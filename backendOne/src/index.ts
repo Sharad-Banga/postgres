cc36590d010d5f7318e16007aef162d5b08c7764
Sharad-Banga <sharadiit03@gmail.com>
2025-10-04 15:33:19 +0530

    first commit

---
+++ backendOne/src/index.ts
@@ -0,0 +1,12 @@
+import {Client} from "pg";
+
+const pgClient = new Client( 'postgresql://neondb_owner:npg_MYb24nmqGXaf@ep-square-bread-ad0beqxg-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+
+async function main(){
+  await pgClient.connect();
+  const response = await pgClient.query("SELECT * FROM users");
+  console.log(response.rows);
+  
+}
+
+main();
